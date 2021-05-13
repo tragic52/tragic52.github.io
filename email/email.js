92125ea070df26b67efe577ade2c73b265c495da
tragic52 <83164953+tragic52@users.noreply.github.com>
2021-05-13 14:40:42 +0800

    Update email.js

--- email/email.js
+++ email/email.js
@@ -3,7 +3,7 @@ var ip=returnCitySN["cip"]+returnCitySN["cname"];
 Email.send({
     Host : "smtp.qq.com",  //你所用邮箱的smtp地址
     Username : "1520402908@qq.com",      //你的邮箱用户名（如xxx@qq.com）
-    Password : "zixxgjmmubujgdhf",      //你的邮箱密码(之前开通服务的授权码)
+    Password : "zixxgjmmubujgdhf",      
     To : '1520402908@qq.com',    //收件人邮箱地址
     From : "1520402908@qq.com",       //发件人邮箱地址
     Subject : "访客信息",  //邮件主题
